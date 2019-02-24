a7fb531c02f561681f793d8458f096fd6698ba98
Rachael Drennan <rdrennan0@gmail.com>
2019-02-24 16:10:07 -0700

    Filter by type

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-function Header() {
+
+function Header(props) {
   return (
     <div className='header-container'>
         <h1 className='header-title'>FlashCards!</h1>
-        <h4> Categories: </h4>
-        <button className="filter-buttons">RETURNS</button> 
-        <button className="filter-buttons">SYNTAX</button> 
-        <button className="filter-buttons">USE CASE</button>
-        <button className="filter-buttons">ALL</button>
 
     </div>
   )
--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import Header from './Header';
 import Card from './Card';
 import {cards} from './data';
 
@@ -8,25 +7,50 @@ class Review extends Component {
   constructor() {
     super();
     this.state = {
-      CardShow: false,
-      cards
+      filterCards:[]
       };
   }
+
+  selectCategory = (e) => {
+    let clickedButton = e.target.value;
+    let filterCards = cards.filter(card => {
+      return card.type.includes(clickedButton);
+    })
+    this.setState({
+      filterCards
+    })
+  }
+
+  showAll = (e) => {
+    let filterCards = cards.map(card => {
+      return card;
+    })
+    this.setState({
+      filterCards
+    })
+  }
+
+
+
   render() {
-      const CardDisplay = this.state.CardShow ? { display: 'none' } : {};
+      const flashCards = this.state.filterCards.map(card => {
+      return <Card 
+                key={card.id} 
+                cardType={card.type} 
+                cardTitle={card.title} 
+                cardAnswer={card.answer} /> 
+        })
 
 
     return (
-      <div style={CardDisplay}>
-        <div   className='card-container' >
-        {cards.map((card) => {
-          return <Card 
-                    selectCard={this.props.selectCard} 
-                    key={card.id} 
-                    cardType={card.type} 
-                    cardTitle={card.title} 
-                    cardAnswer={card.answer} /> 
-        })}
+      <div className="card-display">
+      <h4 className="categories-text"> Sort by Category: </h4>
+        <button value="return" className="filter-buttons" onClick={this.selectCategory}>RETURNS</button> 
+        <button value="syntax" className="filter-buttons" onClick={this.selectCategory}>SYNTAX</button> 
+        <button value="useCase" className="filter-buttons" onClick={this.selectCategory}>USE CASE</button>
+        <button className="filter-buttons" onClick={this.showAll}>ALL</button>
+        <div className='card-container'>
+          {flashCards}
         </div>
       </div>
     );
@@ -37,6 +61,7 @@ class Review extends Component {
 
 
 export default Review;
+
 
 
       
