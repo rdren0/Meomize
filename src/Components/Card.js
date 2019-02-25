4a5c1b58fd65cfcddc556639484a1f5de326268c
Rachael Drennan <rdrennan0@gmail.com>
2019-02-25 13:57:12 -0700

    Toggle cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       showAnswer: false
     }
   }
 
 
-  answerVisible(){
-    this.setState({showAnswer:!showAnswer})
-
+  answerVisible = () => {
+    let show = this.state.showAnswer;
+    console.log(show);
+    this.setState({showAnswer:!show})
   }
 
 render() {
+
+  if(this.state.showAnswer){
     return (
-      <div>
+      <div className= "answer-box">
+            <h2>
+            {this.props.cardAnswer}
+            </h2>
+            <button onClick= {this.answerVisible}>Try Again</button>
+            <button className="remove-card">Delete Card</button>
+      </div>
+        )
+  }else {
+    return (
        <div className="card-box">
           <h3 className="card-title">
             {this.props.cardTitle}
           </h3>
           <button onClick= {this.answerVisible} className="show-answer">Show Answer</button>
         </div>
-        {this.state.showAnswer &&
-        <div className="hidden-answer">
-            <h2>
-            {this.props.cardAnswer}
-            </h2>
-            <button onClick= {this.answerVisible}>Try Again</button>
-            <button className="remove-card">Delete Card</button>
-        </div>}
-      </div>
-    );
+      
+      );
+    }
   }
 }
 
