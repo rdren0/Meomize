7524d43ffd0ed7f2214342822a95e6f9d46d0f05
Rachael Drennan <rdrennan0@gmail.com>
2019-02-25 16:31:39 -0700

    Conditional render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Review from './Components/Review';
 import Header from './Components/Header';
+import logo from './logo.svg';
+
 
 
 import './App.css';
@@ -9,18 +11,21 @@ export class App extends Component {
   constructor() {
     super();
     this.state = {
-      cards: []
+      cards: [],
+      loadingScreen: true
     }
 
     }
 
     componentDidMount() {
+      console.log('hello');
     fetch('http://memoize-datasets.herokuapp.com/api/v1/rdCards')
       .then(response => response.json())
       .then(cards => {
           console.log(cards);
         this.setState({
-          cards: cards.rdCards
+          cards: cards.rdCards,
+          loadingScreen: false
         })
       })
       .catch(err => {
@@ -31,12 +36,25 @@ export class App extends Component {
 
 
   render() {
+    if(this.state.loadingScreen){
+      return(
+        <div>
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Loading Data...          
+          </p>
+        </div>
+
+        )
+    }else{
     return (
       <div className="App">
       <Header />
       <Review cards={this.state.cards} />
       </div>
     );
+      
+    }
   }
 }
 
